bd65d72a58c649f29d7c8a0726a469041e283c0f
Heli <heliribeiro2@gmail.com>
2023-09-15 01:45:15 -0300

    feat: setting up prisma and create users table

---
+++ prisma/migrations/20230915043610_create_users/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "favorite_sport" TEXT NOT NULL
+);
